f6c666ba01b82e4342e3bf9ab1c60c05860615ff
djogo16 <kabala.frank@gmail.com>
2018-09-22 11:39:20 -0600

    cors headed allows all origins

--- backend/settings.py
+++ backend/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000/'
 )
