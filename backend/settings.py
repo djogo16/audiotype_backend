bb777d0d403531382f6183b15d73fa8fefea2456
djogo16 <kabala.frank@gmail.com>
2018-09-22 12:02:48 -0600

    cors headed allows all origins

--- backend/settings.py
+++ backend/settings.py
@@ -73,6 +73,17 @@ CORS_ALLOW_METHODS = (
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000/'
 )
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
